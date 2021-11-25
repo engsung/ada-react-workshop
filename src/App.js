20f98a1eddafbe67717bc141984cbfb84ffb1dcc
Eng Sung <engxsung@gmail.com>
2021-11-25 02:34:12 -0500

    created todoitem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import TodoList from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <TodoList></TodoList> */}
+      <TodoList />
+      <input type="text" value="placeholder"/>
+      <button>Add</button>
+      <button>Clear all</button>
     </div>
   );
 }
